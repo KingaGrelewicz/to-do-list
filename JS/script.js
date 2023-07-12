8236f5f3cdb055bbf6a97c9827414fd94070f1b3
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2023-07-12 17:54:01 +0200

    Initial commit

---
+++ JS/script.js
@@ -0,0 +1,77 @@
+{
+
+    const tasks = [
+        {
+            content: "nagrać lekcję",
+            done: false,
+        },
+        {
+            constent: "zjeść pierogi",
+            done: true,
+        },
+    ];
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    };
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+                <li
+                    ${task.done ? " style=\"text-decoration: line-through\"" : ""}
+                >
+                <button class="js-remove">usuń</button>
+                    ${task.content}
+                </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButton = document.querySelectorAll(".js-remove");
+
+        removeButton.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+
+    };
+
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        };
+
+        addNewTask(newTaskContent);
+
+    };
+
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
+    };
+
+
+    init();
+};
